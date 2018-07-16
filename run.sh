461ca34975c2843d0fabbc8d38366ad7d01cb388
zandy19 <zandy1982@gmail.com>
2018-07-16 09:29:05 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
-#python ./src/pharmacy_counting.py ./input/itcont.txt ./output/top_cost_drug.txt
+#python ./src/pharmacy_counting.py ./input/itcont.txt ./output/top_cost_drug.txt 
+# You can change the input txt file and output txt file as you wish with the following line
 python3 ./src/pharmacy_counting.py ./input/itcont.txt ./output/top_cost_drug.txt
